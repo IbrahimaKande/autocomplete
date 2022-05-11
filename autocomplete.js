8d90b1d0d6b1bb4a0cfd2b9a388c27ce7a8b1d86
Ibrahima KANDE <ibrahimakd00@gmail.com>
2022-05-11 11:04:28 +0200

    node init - method insert - method search - method collectAll

---
+++ autocomplete.js
@@ -0,0 +1,104 @@
+function TrieNode(key) {
+    this.key = key
+    this.parent = null
+    this.children = {}
+    this.end = false //à la place de l'étoile j'ai mis un flag pour déterminer la fin d'un mot
+}
+
+function searchAllWords(node, array) {
+    // base case, if node is at a word, push to output
+    if (node.end) {
+        array.unshift(node.getWord())
+    }
+
+    // iterate through each children, call recursive searchAllWords
+    for (let child in node.children) {
+        searchAllWords(node.children[child], array)
+    }
+}
+  
+//Parcours le noeud parent pour trouver le mot
+TrieNode.prototype.getWord = function() {
+    let output = []
+    let node = this
+
+    while (node !== null) {
+        output.unshift(node.key)
+        node = node.parent
+    }
+
+    return output.join('')
+}
+
+//on implémente Trie avec une simple racine à valeur nulle
+function Trie() {
+    this.root = new TrieNode(null)
+}
+  
+//Insére un mot
+Trie.prototype.insert = function(word) {
+    let node = this.root
+    
+    for(let i = 0; i < word.length; i++) {
+        //vérifier si le caractère existe parmi les enfants
+        if (!node.children[word[i]]) {
+        //s'il n'existe pas on le crée
+            node.children[word[i]] = new TrieNode(word[i])
+        
+        //nous attribuons également le parent au nœud enfant
+            node.children[word[i]].parent = node
+        }
+      
+        //on passe à la profondeur suivante
+        node = node.children[word[i]]
+      
+        //puis on fait un check pour voir si c'est la fin du mot
+        if (i == word.length-1) {
+            //si c'est le cas le flag "end" passe à true
+            node.end = true
+        }
+    }
+}
+  
+//retourne tous les mots commençant par le préfixe "word"
+Trie.prototype.search = function(word) {
+    let node = this.root
+    let output = []
+
+    for(let i = 0; i < word.length; i++) {
+        //on check si ce préfixe à des mots existant dans l'arbre
+        if (node.children[word[i]]) {
+        node = node.children[word[i]]
+        } else {
+        //lorsqu'il y en a pas on retourne "null"
+        return null
+        }
+    }
+
+    //on rappelle la méthode pour parcourir tout l'arbre
+    searchAllWords(node, output)
+
+    return output
+}
+
+//retourne tous les mots existant dans l'arbre
+Trie.prototype.collectAllWords = function(node, words) {
+    searchAllWords(node,words)
+    return words
+}
+
+
+let trie = new Trie()
+
+console.log(trie.collectAllWords(trie.root,[]))
+
+trie.insert("bat")
+trie.insert("battle")
+trie.insert("butter")
+
+console.log(trie.search("ba"))
+console.log(trie.search("batt"))
+
+console.log(trie.collectAllWords(trie.root,[]))
+
+  
